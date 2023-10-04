d9be101434a56fdd49a0b77206f6fb6f69fcb68c
haniladjamba <s22110313@student.unklab.ac.id>
2023-10-04 07:05:21 -0700

    adding pull and push tutorial

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 // Basic Git and Github
+// Learning Github Colaborator
 
+//--------------------------------------------------------------------------------------------------------------------------------
+// BEFORE YOU START LEARNING
+// always check your current remote origin in the git bash
+// git remote -v
+// this will show you the repository you are currently working on
+// make sure that you are in the correct repository
+//--------------------------------------------------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------------------------------------------------
+// PULL and PUSH
+
+// step 0 (clone the reporsitory if you dont have it on your local computer)
+// git clone https://github.com/haniladjamba/latihan-git1
+// then open the file on your text editor
+
+// step 1 (pull the repository first)
+// git pull
+// this should pull all the newest change that been added to the file online
+
+// step 2 (edit the file)
+// Try console.log() your name here
 console.log("Perubahan di hari senin");
 console.log("Poetri cantik hehe");
 console.log("Juan Keren");
@@ -8,3 +30,24 @@ console.log("front end menyenangkan");
 console.log("putri kedua");
 console.log("Hani Jastin Giovanni Ladjamba");
 console.log("Hani Jastin Giovanni Ladjamba 2");
+
+// step 3 (open git bash)
+
+// step 4 (add the changes that you make in the git bash)
+// git add .
+// this will add all the change you make in all of the files in the current folder
+
+// step 5 (commit your changes)
+// git commit -m "Adding my name"
+// always remember to commit before you push the current changes
+
+// step 6 (push the current changes to the online repository)
+// git push
+
+// Done
+//--------------------------------------------------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------------------------------------------------
+// BEST PRACTICE
+// the best practice when working on collaboration is to always remember to PULL first then PUSH
+//--------------------------------------------------------------------------------------------------------------------------------
