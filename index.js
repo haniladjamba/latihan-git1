a86a781cf04cb2e76370b5a37b2e1caa22457959
03Res <s22110238@student.unklab.ac.id>
2023-10-06 00:16:51 +0800

    resma

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ console.log("Hani Jastin Giovanni Ladjamba");
 console.log("Hani Jastin Giovanni Ladjamba 2");
 console.log("halohai, i'm natalia");
 console.log("ini kedua");
-
+console.log("jadi gini...")
 // step 3 (open git bash)
 
 // step 4 (add the changes that you make in the git bash)
